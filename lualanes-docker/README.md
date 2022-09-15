7f28be1f081c2668eb28bf7ee6482c2a2602d1a3
J. Eduardo <j.eduardo@gmail.com>
2022-09-15 09:19:46 +0200

    Added test code for Lua lanes

---
+++ lualanes-docker/README.md
@@ -0,0 +1,3 @@
+# Lua Lanes in Docker
+
+Simple test code to run a Lua script using [Lua Lanes](https://lualanes.github.io/lanes/) inside of Docker.
---
+++ lualanes-docker/test.lua
@@ -0,0 +1,7 @@
+#!/usr/bin/env lua5.1
+
+print('Loading and initializing lanes')
+
+require('lanes').configure()
+
+print('If I got here, lanes initialized successfully')
