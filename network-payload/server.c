9fcfd0f4beadfbf4af247bacf73ea92c6305eb81
J. Eduardo <j.eduardo@gmail.com>
2024-01-31 09:55:52 +0100

    Sending back only the bytes that were read

--- network-payload/server.c
+++ network-payload/server.c
@@ -78,7 +78,7 @@ int main(int argc, char *argv[]) {
         if (n < 0) error("ERROR reading from socket");
         printf("<- %d bytes read\n", n);
 
-        n = write(newsockfd, buffer, buf_size);
+        n = write(newsockfd, buffer, n);
         if (n < 0) error("ERROR writing to socket");
         printf("-> %d bytes written\n", n);
 
